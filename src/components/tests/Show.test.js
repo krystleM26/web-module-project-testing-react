d7aa7d6fdef7cdbeff49fd84c29bb3e79e796d5d
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-01-24 15:59:59 -0800

    built show data

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,17 +5,36 @@ import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
 
+const showData = {
+    name: '',
+    summary: '',
+    seasons:[
+        {
+
+            id: 0,
+            name: '',
+            episodes: []
+        }
+    ]
+}
+
 test('renders without errors', ()=>{
-    render(<Show/>)
+    render(<Show show={showData}/>)
 });
 
 test('renders Loading component when prop show is null', () => {
     render(<Show show={null}/>)
     const loading = screen.queryByText('Fetching Data...')
+  
+    
+  
 });
 
 
-test('renders same number of options seasons are passed in', ()=>{});
+test('renders same number of options seasons are passed in', ()=>{
+    render(<Show/>);
+
+});
 
 test('handleSelect is called when an season is selected', () => {});
 
